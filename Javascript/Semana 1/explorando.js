ee1992cea6a30ce6cdb96c91661d33f56b1f78e8
Ema-cr <gaviriacardonaemanuel@gmail.com>
2025-06-17 13:42:02 -0500

    Update explorando.js

--- Javascript/Semana 1/explorando.js
+++ Javascript/Semana 1/explorando.js
@@ -1,5 +1,5 @@
 let nombre = "emanuel";
-console.log(`Hola, ${nombre}`);
+console.log(`Hola,  ${nombre}`);
 
 let entero = 1;
 console.log(entero)
@@ -13,8 +13,8 @@ console.log(texto)
 const PI = 3.1416;
 console.log(PI)
 
-// PI = 3.9586;
-// console.log(PI)
+//PI = 3.9586;
+//console.log(PI)
 
 let klk = "";
 console.log(klk)
@@ -40,10 +40,40 @@ console.log("suma", numero1 + numero2);
 console.log("resta", numero1 - numero2);
 console.log("multiplicacion", numero1 * numero2);
 console.log("division", numero1 / numero2);
-console.log("modulo", numero2 % numero1);
+console.log("modulo", numero1 % numero2);
 
 let saludo = "Hola";
 let nombresinho = "Emanuel";
 
-let mensaje = saludo + "" + nombresinho;
+let mensaje = saludo + " " + nombresinho;
 console.log(mensaje);
+
+
+console.log(5 == "5");
+console.log(5 === "5");
+console.log(true && false);
+console.log(false || true);
+console.log(!true);
+
+
+
+let numero_user = prompt("Cual es tu numero?")
+
+if (numero_user < 10){
+    console.log('Es menor')
+} else if (numero_user > 10){
+    console.log('Es mayor')
+} else{
+    console.log('Es igual')
+}
+
+let user = prompt("Cual es tu nombre de usuario?")
+
+if (user === "Admin"){
+    console.log('Bienvenido Admin eres el mejor')
+} else{
+    console.log('Bienvenido random')
+}
+
+let resultado = prompt('Ingresa tu numero')
+
