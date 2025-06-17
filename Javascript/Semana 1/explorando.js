47d0b640a199fcfed379c06740ce6663705fe391
Ema-cr <gaviriacardonaemanuel@gmail.com>
2025-06-16 21:32:27 -0500

    Update explorando.js

--- Javascript/Semana 1/explorando.js
+++ Javascript/Semana 1/explorando.js
@@ -1,5 +1,5 @@
-let nombre = "Emanuel";
-console.log(nombre)
+let nombre = "emanuel";
+console.log(`Hola, ${nombre}`);
 
 let entero = 1;
 console.log(entero)
@@ -21,3 +21,29 @@ console.log(klk)
 
 let aino = null;
 console.log(aino)
+
+let verdadero = true;
+
+let edad = prompt("¿Cuál es tu edad?");
+console.log(`Tienes ${edad} años`);
+
+alert("¡Bienvenid@ a nuestra página!");
+
+
+let deseasContinuar = confirm("¿Deseas continuar?");
+console.log(deseasContinuar);
+
+let numero1 = 24;
+let numero2 = 4;
+
+console.log("suma", numero1 + numero2);
+console.log("resta", numero1 - numero2);
+console.log("multiplicacion", numero1 * numero2);
+console.log("division", numero1 / numero2);
+console.log("modulo", numero2 % numero1);
+
+let saludo = "Hola";
+let nombresinho = "Emanuel";
+
+let mensaje = saludo + "" + nombresinho;
+console.log(mensaje);
