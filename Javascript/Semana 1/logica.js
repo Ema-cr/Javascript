362b647242eeb61359f0e203707c4629f3836afa
Ema-cr <gaviriacardonaemanuel@gmail.com>
2025-06-18 13:49:25 -0500

    Add files via upload

--- Javascript/Semana 1/logica.js
+++ Javascript/Semana 1/logica.js
@@ -14,3 +14,38 @@ if (suma >= lado3) {
 }else{
     console.error("No es un triangulo valido")
 }
+
+let frutas=["fresa", "manzana", "naranja", "sandia"]
+let primero = frutas.shift();
+console.log(primero)
+
+let numeros=[1,2,3,4,5]
+console.log(numeros);
+let numero_final = numeros.push(24);
+console.log(numeros);
+
+let palabras=["Mauricio","Maracaibo","Mardicion","Manolete"]
+console.log(palabras);
+let principio = palabras.unshift("Mardito");
+console.log(palabras);
+
+let colores=["Negro","Blanco","Azul","Naranjado"]
+console.log(colores)
+let ultimo = colores.pop()
+console.log(colores);
+
+let extraido = colores[1];
+console.log(extraido)
+
+let listanumeros = [1,2,3,4,5,6,7,8,9,10]
+console.log(listanumeros);
+let extraidos = listanumeros.slice(0,5)
+console.log(extraidos);
+
+console.log(palabras)
+palabras.splice(1, "Chamos")
+console.log(palabras)
+
+let palabras_list = ["La","Venganza","De","Los","Chamos", 2 ,"Chavez","La","Resureccion"]
+let texto_comp = palabras_list.join(" ")
+console.log(texto_comp)
