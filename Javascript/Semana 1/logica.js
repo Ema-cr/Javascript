babc66129d3171e7160a0ada80286dd32645b22b
Ema-cr <gaviriacardonaemanuel@gmail.com>
2025-06-18 22:39:57 -0500

    Update logica.js

--- Javascript/Semana 1/logica.js
+++ Javascript/Semana 1/logica.js
@@ -49,3 +49,28 @@ console.log(palabras)
 let palabras_list = ["La","Venganza","De","Los","Chamos", 2 ,"Chavez","La","Resureccion"]
 let texto_comp = palabras_list.join(" ")
 console.log(texto_comp)
+
+palabras_list.sort();
+console.log(palabras_list)
+
+palabras_list.reverse();
+console.log(palabras_list)
+
+console.log(palabras_list.includes("Venganza"))
+console.log(palabras_list.includes("Perrito"))
+
+let palabrinha = "mamagvo"
+for(const element of palabrinha){
+  console.log(element);
+}
+
+let str = ""
+for (let i = 0; i < 6; i++){
+  str += i;
+}
+console.log(str);
+
+let palabrota = "AINO"
+for(const element in palabrota){
+  console.log(`${element}: ${palabrota[element]}`);
+}
